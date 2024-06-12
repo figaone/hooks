ef8a8d2bf2e5cd1b88d7677689c6ffb697378f43
kojo Adu-Gyamfi <adugyamfikonadu48@gmail.com>
2024-06-12 05:07:55 -0500

    add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
 
   let  [count,setCount] = useState(0)
+  let [animal,setAnimal] = useState('Dog')
 
   const addOne = () => setCount(count + 1);
   const restOne = () => setCount(count - 1);
   const setZero = () => setCount(0);
+
+  const doApicall = () => {
+    console.log('go to server and get data')
+  }
+
+  useEffect(() => {
+    console.log('run once')
+  }, [])
+
+
+  // useEffect(()=>{
+  //  console.log(count)
+  // },[count])
+
+  // useEffect(()=>{
+  //   console.log(animal)
+  //  },[animal])
+
 
   return(
     <div>
@@ -14,6 +33,9 @@ const App = () => {
       <button onClick={addOne}>+1</button>
       <button onClick={restOne}>-1</button>
       <button onClick={setZero}>Set to 0</button>
+      <hr/>
+      <h3>{animal}</h3>
+      <button onClick={ () => setAnimal('Cat')}>Change Animal</button>
     </div>
   )
 }
